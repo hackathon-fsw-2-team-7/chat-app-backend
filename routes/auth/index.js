bed08907b3fb18b29a1fcaa787c4eb0e1f85f6c3
Hasan Nur Wakhid <hasannurwakhid@gmail.com>
2024-05-17 16:41:47 +0700

    change route

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const registerUser = require("../../controllers/register/index");
-const {login, googleLogin} = require("../../controllers/auth/index.js");
-const {profile} = require("../../controllers/profile/index");
-const {authMiddleware} = require("../../middlewares/auth");
+const { login, googleLogin } = require("../../controllers/auth/index.js");
+const { profile } = require("../../controllers/profile/index");
+const { authMiddleware } = require("../../middlewares/auth");
 
-router.post("/login", login).post("/google-login", googleLogin)
-    .post("/register", registerUser.register).post("/profile", authMiddleware, profile);
+router
+  .post("/login", login)
+  .post("/google-login", googleLogin)
+  .post("/register", registerUser.register)
+  .post("/profile", authMiddleware, profile);
 
 module.exports = router;
